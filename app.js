a479d60d5a8d6f28932f388bc01363dd304419e2
Acedenos <ana.cedeno81@gmail.com>
2025-03-16 21:11:18 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log ("Ejecutando el sistema")
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = []
+
+function agregarAmigo() {
+
+    let nombreAmigo = document.getElementById('amigo').value;
+    if (nombreAmigo !== "") {
+        amigos.push (nombreAmigo);
+        crearLista(nombreAmigo);
+        amigo.value = "";
+
+    } else {
+        alert ('Por favor inserte un nombre.');  
+    }
+
+}
+
+function crearLista(nombreAmigo) {
+    
+    let ul_lista = document.getElementById('listaAmigos');
+    let li = document.createElement('li');
+    li.textContent = nombreAmigo;
+    ul_lista.appendChild (li);
+
+}
+
+
+    
+
+
+
